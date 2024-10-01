b7320df32b66cb70113a8d879239d26508b3699e
Paulo GS2024 <164101438+PGS2024@users.noreply.github.com>
2024-09-30 21:28:53 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,55 @@
+/*let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+numeros[numeros.length] = 10;
+console.log(numeros);*/
+
+/*let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+numeros[numeros.length] = 10;
+
+numeros.push(11)
+numeros.push(12, 13)
+
+console.log(numeros)*/
+
+/*let frutas = ["maçâ", "banana", "laranja"];
+let stringFrutas = frutas.join(", ");
+
+console.log(stringFrutas);*/ 
+
+/*let numeros = [40, 100, 1, 5, 25, 10];
+numeros.sort();
+console.log(numeros);*/
+
+/*let letras = ["a", "b", "c", "d"];
+letras.reverse()
+
+console.log(letras);*/
+
+/*let matriz = [
+    [1, 2, 3],
+    [4, 5, 6]
+];
+
+console.log(matriz[0][1]);
+
+matriz[1][2] = 10;
+console.log(matriz[1][2]);
+
+for (let i = 0; i < matriz.length; i++){
+    for (let j = 0; j < matriz[i].length; j++) {
+        console.log(`Elemento na posição [${i}][${j}] = ${matriz[i][j]}`);
+    }
+}*/
+
+let matrizx3D = [
+    [
+        [1, 2],
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+];
+
+console.log(matrizx3D[0][1][0]);
+
